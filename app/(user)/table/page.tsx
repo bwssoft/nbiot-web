4d6a519cfd680eabbfea4a310c18c710ba5c3aca
Rodrigo Ribeiro <rdo.rodrigo2@gmail.com>
2024-05-28 10:55:11 -0300

    feat: add table serial number filtering

--- app/(user)/table/page.tsx
+++ app/(user)/table/page.tsx
@@ -1,29 +1,41 @@
 // import { listMany } from "@/app/lib/action/last_package";
 import { listMany } from "@/app/lib/action/search_package";
 
+import { ILastPackage } from "@/app/lib/definition/last_package";
 import { LastPackageTable } from "@/app/ui/@pages/tables/last_package";
-import { InputComponent } from "@/app/ui/input/index";
-import { Button } from "@bwsoft/button";
+import { Filter } from "mongodb";
+import { SerialTableFilter } from "./filter";
 
-export default async function Table() {
-  const packages = await listMany({});
+interface Params {
+  searchParams: {
+    serialNumber?: string;
+  }
+}
+
+export default async function Table({ searchParams }: Params) {
+  async function fetchPackages() {
+    const query: Filter<ILastPackage> = {};
+
+    if (searchParams.serialNumber) {
+      query['serialNumber'] = Number(searchParams.serialNumber);
+    }
+
+    return await listMany(query);
+  }
+
+  const packages = await fetchPackages();
+  
   return (
-    <main className="grid grid-rows-[min-content_1fr] gap-4 p-24">
-      <div className="mb-12">
-        <h1 className="mb-4">Últimos dados recebidos</h1>
+    <main className="gap-4 flex justify-center p-8">
+      <div className="grid gap-4 container grid-rows-[min-content_1fr] ">
         <div>
-          <form className="flex gap-4">
-            <InputComponent />
-            <Button
-              type="submit"
-              className=" w-20 bg-indigo-200 text-indigo-700"
-            >
-              Buscar
-            </Button>
-          </form>
+          <h1 className="mb-4">Últimos dados recebidos</h1>
+          <div>
+            <SerialTableFilter />
+          </div>
         </div>
+        <LastPackageTable data={packages} />
       </div>
-      <LastPackageTable data={packages} />
     </main>
   );
 }
