1d6f6ded87aff332fa7921eee18870fa68564a3e
devitalo97 <dev.italo.souza@gmail.com>
2024-06-02 19:57:41 -0300

    feat: implement request with query on map page

--- app/(user)/map/page.tsx
+++ app/(user)/map/page.tsx
@@ -1,16 +1,40 @@
-import { listMany } from "@/app/lib/action/last_package";
+import { listMany } from "@/app/lib/action/search_package";
 import { IPackage } from "@/app/lib/definition/package";
 import Map from "@/app/ui/@pages/map";
+import { SerialMapFilter } from "./filter";
+import { Filter } from "mongodb";
+import { ILastPackage } from "@/app/lib/definition/last_package";
 
-export default async function MapPage() {
-  const packages = await listMany();
+interface Params {
+  searchParams: {
+    serialNumber?: string;
+  };
+}
+
+export default async function MapPage({ searchParams }: Params) {
+  let query: Filter<ILastPackage> = {};
+
+  if (searchParams.serialNumber) {
+    query = { serialNumber: { $regex: searchParams.serialNumber } };
+  }
+
+  const packages = await listMany(query);
+
   return (
-    <main>
-      <Map
-        packages={
-          packages as IPackage<{ latitude: number; longitude: number }>[]
-        }
-      />
+    <main className="gap-4 flex justify-center p-8">
+      <div className="grid gap-4 container grid-rows-[min-content_1fr] ">
+        <div>
+          <h1 className="mb-4">Mapa</h1>
+          <div>
+            <SerialMapFilter />
+          </div>
+        </div>
+        <Map
+          packages={
+            packages as IPackage<{ latitude: number; longitude: number }>[]
+          }
+        />
+      </div>
     </main>
   );
 }
